7a4dc7dd5d26d0d1cbbd4efe2e1b3155f8caaac0
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2021-06-26 16:07:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,127 +1,155 @@
-// PLAYER SECTION //
-function getVideo(index) {
-  var http1 = new XMLHttpRequest();
-  http1.open(
-    "GET",
-    "https://5d76bf96515d1a0014085cf9.mockapi.io/video/" + index,
-    true
-  );
-  http1.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      var responceVideo = JSON.parse(this.responseText);
-      videoFrame.src =
-        "https://player.vimeo.com/video/" + responceVideo.vimeoId;
-      vidView.innerHTML = responceVideo.views + " views";
-      vidName.innerHTML = responceVideo.title;
-      vidDetails.innerHTML = responceVideo.description;
-      document.body.scrollTop = 80;
-      document.documentElement.scrollTop = 80;
+$(document).ready(function () {
+  // PLAYER SECTION //
+  var playerSection = $("#player-section");
+  var mainPlayerDiv = $("<div>").attr("id", "player-div");
+  var videoFrame = $("<iframe>")
+    .attr("id", "video-player")
+    .attr("frameBorder", 0);
+  videoFrame.attr("webkitallowfullscreen");
+  videoFrame.attr("mozallowfullscreen");
+  videoFrame.attr("allowfullscreen");
+  var vidDe = $("<div>").attr("id", "vid-details");
+  var vidView = $("<p>");
+  var iconDiv = $("<div>").attr("id", "ico");
+
+  var icoHeart = $("<i>").attr("class", "far fa-heart");
+  icoHeart.click(function () {
+    var fileId = this.id;
+    if (this.className.indexOf("liked") != -1) {
+//       updateLike(fileId, false);
+      icoHeart.attr("class", "far fa-heart notLiked");
+    } else {
+//       updateLike(fileId, true);
+      icoHeart.attr("class", "far fa-heart liked");
     }
-  };
-  http1.send();
-}
-
-var playerSection = document.getElementById("player-section");
-
-var mainPlayerDiv = document.createElement("div");
-mainPlayerDiv.id = "player-div";
-
-var videoFrame = document.createElement("iframe");
-videoFrame.id = "video-player";
-videoFrame.frameBorder = 0;
-videoFrame.webkitallowfullscreen;
-videoFrame.mozallowfullscreen;
-videoFrame.allowfullscreen;
-
-var vidDe = document.createElement("div");
-vidDe.id = "vid-details";
-
-var vidView = document.createElement("p");
-
-var iconDiv = document.createElement("div");
-iconDiv.id = "ico";
-
-var icoHeart = document.createElement("i");
-icoHeart.className = "far fa-heart";
-var icoComment = document.createElement("i");
-icoComment.className = "far fa-comment-alt";
-var icoBookmark = document.createElement("i");
-icoBookmark.className = "far fa-bookmark";
-
-iconDiv.append(icoHeart);
-iconDiv.append(icoComment);
-iconDiv.append(icoBookmark);
-
-vidDe.append(vidView);
-vidDe.append(iconDiv);
-
-var divVid = document.createElement("div");
-divVid.id = "vid-d";
-
-var vidName = document.createElement("h1");
-
-var vidDetails = document.createElement("p");
-vidDetails.id = "vid-des";
-
-divVid.append(vidName);
-divVid.append(vidDetails);
-
-mainPlayerDiv.append(videoFrame);
-mainPlayerDiv.append(vidDe);
-mainPlayerDiv.append(divVid);
-
-playerSection.append(mainPlayerDiv);
-getVideo(1);
-
-// PLAYLIST SECTION //
-function createPlaylistCard(imgSrc, vidNme, idC) {
-  var card = document.createElement("div");
-  card.id = "card " + idC;
-  if (card.id == "card 1") {
-    card.className = "playlist-card active-card";
-  } else {
-    card.className = "playlist-card";
-  }
-
-  var imageTag = document.createElement("img");
-  imageTag.className = "thumbnail";
-  imageTag.src = imgSrc;
-
-  var headingVid = document.createElement("h3");
-  headingVid.innerHTML = vidNme;
-
-  card.append(imageTag);
-  card.append(headingVid);
-
-  playlistDiv.append(card);
-
-  card.addEventListener("click", function () {
-    idCard = card.id;
-    idCard = idCard.split(" ");
-    getVideo(idCard[1]);
-    var lastActive = document.getElementsByClassName("active-card");
-    lastActive[0].className = "playlist-card";
-    card.className = "playlist-card active-card";
   });
-}
 
-var playlistDiv = document.createElement("div");
-playlistDiv.id = "playlist-wrapper";
-playerSection.append(playlistDiv);
-
-var http = new XMLHttpRequest();
-http.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist", true);
-http.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    var responcePlaylist = JSON.parse(this.responseText);
-
-    for (var i = 0; i < responcePlaylist.length; i++) {
-      createPlaylistCard(
-        responcePlaylist[i].thumbnail,
-        responcePlaylist[i].title,
-        i + 1
-      );
-    }
+  // Function to update likes
+  function updateLike(fileId, value) {
+    $.ajax({
+      type: "PUT",
+      url: "https://5d76bf96515d1a0014085cf9.mockapi.io/video/" + fileId,
+      dataType: "json",
+      data: { isLiked: value },
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (request, status, errorThrown) {
+        console.log(request);
+        console.log(status);
+        console.log(errorThrown);
+      },
+    });
   }
-};
-http.send();
+
+  var icoComment = $("<i>").attr("class", "far fa-comment-alt");
+
+  var icoBookmark = $("<i>").attr("class", "far fa-bookmark");
+  icoBookmark.click(function () {
+    var fileId = this.id;
+    if (this.className.indexOf("saved") != -1) {
+//       updateSave(fileId, false);
+      icoBookmark.attr("class", "far fa-bookmark notSaved");
+    } else {
+//       updateSave(fileId, true);
+      icoBookmark.attr("class", "far fa-bookmark saved");
+    }
+  });
+
+  // Function to update saves
+  function updateSave(fileId, value) {
+    $.ajax({
+      type: "PUT",
+      url: "https://5d76bf96515d1a0014085cf9.mockapi.io/video/" + fileId,
+      dataType: "json",
+      data: { isSaved: value },
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (request, status, errorThrown) {
+        console.log(request);
+        console.log(status);
+        console.log(errorThrown);
+      },
+    });
+  }
+
+  iconDiv.append(icoHeart, icoComment, icoBookmark);
+  vidDe.append(vidView, iconDiv);
+
+  var divVid = $("<div>").attr("id", "vid-d");
+  var vidName = $("<h1>");
+  var vidDetails = $("<p>").attr("id", "vid-des");
+
+  divVid.append(vidName, vidDetails);
+  mainPlayerDiv.append(videoFrame, vidDe, divVid);
+  playerSection.append(mainPlayerDiv);
+  getVideo(1);
+
+  function getVideo(index) {
+    $.get(
+      "https://5d76bf96515d1a0014085cf9.mockapi.io/video/" + index,
+      function (response) {
+        videoFrame.attr(
+          "src",
+          "https://player.vimeo.com/video/" + response.vimeoId
+        );
+        var views = parseInt(response.views);
+        views = views / 1000 + "k";
+        vidView.html(views + " views");
+        vidName.html(response.title);
+        vidDetails.html(response.description);
+        icoHeart.attr("id", response.id);
+        if (response.isLiked === "true") {
+          icoHeart.attr("class", "far fa-heart liked");
+        } else {
+          icoHeart.attr("class", "far fa-heart notLiked");
+        }
+
+        if (response.isSaved === "true") {
+          icoBookmark.attr("class", "far fa-bookmark saved");
+        } else {
+          icoBookmark.attr("class", "far fa-bookmark notSaved");
+        }
+        document.body.scrollTop = 80;
+        document.documentElement.scrollTop = 80;
+      }
+    );
+  }
+
+  // PLAYLIST SECTION //
+  function createPlaylistCard(imgSrc, vidNme, idC) {
+    var card = $("<div>").attr("id", "card " + idC);
+    if (card.attr("id") == "card 1") {
+      card.attr("class", "playlist-card active-card");
+    } else {
+      card.attr("class", "playlist-card");
+    }
+
+    var imageTag = $("<img>").attr("class", "thumbnail").attr("src", imgSrc);
+    var headingVid = $("<h3>").attr("class", "playlistHeading").html(vidNme);
+
+    card.append(imageTag, headingVid);
+    playlistDiv.append(card);
+
+    card.click(function () {
+      idCard = card.attr("id");
+      idCard = idCard.split(" ");
+      getVideo(idCard[1]);
+      $(".active-card").attr("class", "playlist-card");
+      card.attr("class", "playlist-card active-card");
+    });
+  }
+
+  var playlistDiv = $("<div>").attr("id", "playlist-wrapper");
+  playerSection.append(playlistDiv);
+
+  $.get(
+    "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist",
+    function (response) {
+      for (var i = 0; i < response.length; i++) {
+        createPlaylistCard(response[i].thumbnail, response[i].title, i + 1);
+      }
+    }
+  );
+});
